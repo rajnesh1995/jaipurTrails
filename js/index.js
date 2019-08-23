dfc6bb4cdc51de25f8fd10bf3beb530a69c5a996
rajnesh1995 <rajnesh@iitk.ac.in>
2019-08-23 08:51:19 +0530

    Jaipur Trails

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function logo_change_white(){
+  document.getElementById('logoimage').src="images/logo-white.png"
+}
+
+function logo_change_black(){
+  document.getElementById('logoimage').src="images/logo.png"
+}
